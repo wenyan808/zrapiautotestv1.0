c2234500705b135f2aeb8644b31001192ddfbf84
易首全 <15989434843@163.com>
2020-06-30 14:37:40 +0800

    新增港股相关和股价提醒相关、行情数据修复相关模块，完善一些日K的用例

--- Common/assertapi.py
+++ Common/assertapi.py
@@ -1,5 +1,3 @@
 def assert_data(response, code, msg):
     assert code == response.json().get("code")
     assert msg == response.json().get("msg")
-    if "data" in response.json():
-        assert response.json().get("data")
