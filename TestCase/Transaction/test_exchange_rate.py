617f387b1279a9792ed41836f97e473af0c9f6db
易首全 <15989434843@163.com>
2020-07-27 18:37:20 +0800

    修改获取两个货币的汇率比率

--- TestCase/Transaction/test_exchange_rate.py
+++ TestCase/Transaction/test_exchange_rate.py
@@ -13,33 +13,58 @@ class TestExchangeRate:
 
     @allure.story('获取两个货币的汇率比率_USD&HKD')
     def test_exchange_rate_USD_and_HKD(self):
-        response = zhuorui('个股详情公共接口', '获取两个货币的汇率比率_USD&HKD')
+        response = zhuorui('交易', '获取两个货币的汇率比率_USD&HKD')
         assert_data(response, '000000', 'ok')
+        assert response.status_code == 200
+        # print(response.json())
 
     @allure.story('获取两个货币的汇率比率_USD&CNY')
     def test_exchange_rate_USD_and_CNY(self):
-        response = zhuorui('个股详情公共接口', '获取两个货币的汇率比率_USD&CNY')
+        response = zhuorui('交易', '获取两个货币的汇率比率_USD&CNY')
         assert_data(response, '000000', 'ok')
+        assert response.status_code == 200
+        # print(response.json())
+        if "data" in response.json():
+            # assert response.json().get("data")
+            # print(response.json().get("data").get("rate"))
+            if len(response.json().get("data")) != 0:
+                assert "rate" in response.json().get("data")
+
+
 
     @allure.story('获取两个货币的汇率比率_HKD&CNY')
     def test_exchange_rate_HKD_and_CNY(self):
-        response = zhuorui('个股详情公共接口', '获取两个货币的汇率比率_HKD&CNY')
+        response = zhuorui('交易', '获取两个货币的汇率比率_HKD&CNY')
         assert_data(response, '000000', 'ok')
+        assert response.status_code == 200
+        # print(response.json())
+        if "data" in response.json():
+            # assert response.json().get("data")
+            # print(response.json().get("data").get("rate"))
+            if len(response.json().get("data")) != 0:
+                assert "rate" in response.json().get("data")
 
     @allure.story('获取两个货币的汇率比率_currencyA')
     def test_exchange_rate_currencyA(self):
-        response = zhuorui('个股详情公共接口', '获取两个货币的汇率比率_currencyA')
-        assert_data(response, '000000', 'ok')
+        response = zhuorui('交易', '获取两个货币的汇率比率_currencyA')
+        assert response.status_code == 200
+        assert_data(response, '000103', 'currencyB is null')
+        # print(response.json())
+
 
     @allure.story('获取两个货币的汇率比率_currencyB')
     def test_exchange_rate_currencyB(self):
-        response = zhuorui('个股详情公共接口', '获取两个货币的汇率比率_currencyB')
-        assert_data(response, '000000', 'ok')
+        response = zhuorui('交易', '获取两个货币的汇率比率_currencyB')
+        assert response.status_code == 200
+        assert_data(response, '000103', 'currencyA is error')
+        # print(response.json())
 
     @allure.story('获取两个货币的汇率比率')
     def test_exchange_rate(self):
-        response = zhuorui('个股详情公共接口', '获取两个货币的汇率比率')
-        assert_data(response, '000000', 'ok')
+        response = zhuorui('交易', '获取两个货币的汇率比率')
+        assert response.status_code == 200
+        assert_data(response, '000103', 'currencyB is null')
+        # print(response.json())
 
 
 if __name__ == '__main__':
