dd1d09c5ceaa3bbba28d89d79a821e90342ba1cf
chenjialuo <chenjialuo@gmail.com>
2021-01-12 14:43:40 +0800

    更新run文件（cjl）

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 import os
 
-import pytest
+
 
 if __name__ == '__main__':
-    pytest.main(['--alluredir', '../allure-xml'])
     os.system('pytest --alluredir=./report/xml')
     os.system('allure generate ./report/xml -o ./report/html --clean')
