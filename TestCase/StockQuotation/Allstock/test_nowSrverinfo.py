4bdb7b41f2d4088ab139c2d6af338e7769cce183
易首全 <15989434843@163.com>
2021-01-18 18:30:07 +0800

    新增获取服务器当前的时间戳用例

--- TestCase/StockQuotation/Allstock/test_nowSrverinfo.py
+++ TestCase/StockQuotation/Allstock/test_nowSrverinfo.py
@@ -11,7 +11,6 @@ from Common.login import login
 
 from Common.sign import get_sign
 
-
 from Common.tools.read_yaml import yamltoken
 from glo import HTTP, JSON
 
@@ -31,6 +30,42 @@ class TestNowSrverinfo:
 
         # 拼装参数
         paylo = {}
+        # paylo = info
+        # print(paylo)
+        sign1 = {"sign": get_sign(paylo)}  # 把参数签名后通过sign1传出来
+        # 调用登录接口通过token传出来
+        payload1 = {}
+        payload1.update(paylo)
+        payload1.update(sign1)
+        headers = headers
+        # print(token)
+        # print(type(token))
+
+        token1 = yamltoken()
+        token = {"token": token1}
+        headers.update(token)
+        # print(headers)
+        payload = json.dumps(dict(payload1))
+        # time.sleep(1)
+        r = requests.post(url=url, headers=headers, data=payload)
+        # 断言
+        j = r.json()
+        # print(j)
+        assert r.status_code == 200
+        if j.get("code") == "000000":
+            assert j.get("code") == "000000"
+            assert j.get("msg") == "ok"
+            if "data" in j:
+                assert type(j.get("data")) == type(TimeTostamp())
+
+    @allure.story('获取服务器当前的时间戳-newYork')
+    def test_nowSrverinfo_newYork(self):
+        # pass
+        url = HTTP + "/as_market/api/server_info/v1/now"
+        headers = JSON
+
+        # 拼装参数
+        paylo = {"newYork": "true"}
         # paylo = info
         # print(paylo)
         sign1 = {"sign": get_sign(paylo)}  # 把参数签名后通过sign1传出来
