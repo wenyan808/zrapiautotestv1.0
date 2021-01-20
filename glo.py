eb12ee902b0a17bc708b8eadcb447de866cd73f3
易首全 <15989434843@163.com>
2021-01-20 18:35:22 +0800

    新增glo参数

--- glo.py
+++ glo.py
@@ -5,14 +5,19 @@ FALG = True  # 社区，如果为True则不执行time.sleep(),反之则执行
 headPhoto = 'http://zhuorui-public.oss-cn-shenzhen.aliyuncs.com/head_photo/images/2021/01/11/16103477341540274.jpeg'  # 用户头像发生变化，请在这里修改头像地址
 BASE_DIR: Union[bytes, str] = os.path.dirname(os.path.abspath(__file__))
 HTTP = "http://192.168.1.241"
-console_HTTP = "http://192.168.1.239:8080"
-# JSON = {
-#     "Content-Type": "application/json",
-#     "appVersion": '0.1.4',
-#     "deviceId": "d41b071ca83ece28",
-#     "osType": "android",
-#     "osVersion": '7.1.2'
-# }
+console_HTTP = "http://192.168.1.239:8080/apisC"  # http://192.168.1.241:1216/
+loginAccount = "test@123.com"
+password = "abcd1234567"
+console_JSON = {
+    "Content-Type": "application/json;charset=UTF-8",
+    "Connection": "keep-alive",
+    "traceId": "862c0ed0-5aef-11eb-8806-81a3e151bf57",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko)"
+                  " Chrome/86.0.4240.183 Safari/537.36",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.9"
+}
 # 测试环境192.168.1.241（iOS）('18379204795', '102522ql', '86')
 phone = "18379204795"
 pwd = "102522ql"
