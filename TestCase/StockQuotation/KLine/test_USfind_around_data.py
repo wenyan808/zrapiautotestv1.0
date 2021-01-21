fb4fe870355bcfba14bc42b384827718f2488f89
chenjialuo <chenjialuo@gmail.com>
2021-01-21 16:38:49 +0800

    新增美股分时盘前盘后用例（cjl）

---
+++ TestCase/StockQuotation/KLine/test_USfind_around_data.py
@@ -0,0 +1,25 @@
+import allure
+
+from Common.assertapi import assert_data
+from Common.guide import zhuorui
+from Common.login import login
+
+
+@allure.feature('搜索')
+class TestKeywordSearch:
+    @classmethod
+    def setup_class(cls) -> None:
+        login()
+
+    @allure.story('美股分时盘前查询')
+    def test_USBEFROE(self):
+        response = zhuorui('k线', '美股分时盘前查询')
+        # assert_data(response, '000000', 'ok')
+        print(response.json())
+
+    @allure.story('美股分时盘后查询')
+    def test_USAFTER(self):
+        response = zhuorui('k线', '美股分时盘后查询')
+        # assert_data(response, '000000', 'ok')
+        print(response.json())
+
