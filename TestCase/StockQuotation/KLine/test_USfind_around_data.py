fa028f7a533d98defbca0ab9438da991cf0b8809
易首全 <15989434843@163.com>
2021-01-22 17:59:09 +0800

    修复bug

--- TestCase/StockQuotation/KLine/test_USfind_around_data.py
+++ TestCase/StockQuotation/KLine/test_USfind_around_data.py
@@ -14,12 +14,12 @@ class TestKeywordSearch:
     @allure.story('美股分时盘前查询')
     def test_USBEFROE(self):
         response = zhuorui('k线', '美股分时盘前查询')
-        # assert_data(response, '000000', 'ok')
-        print(response.json())
+        assert_data(response, '000000', 'ok')
+        # print(response.json())
 
     @allure.story('美股分时盘后查询')
     def test_USAFTER(self):
         response = zhuorui('k线', '美股分时盘后查询')
-        # assert_data(response, '000000', 'ok')
-        print(response.json())
+        assert_data(response, '000000', 'ok')
+        # print(response.json())
 
