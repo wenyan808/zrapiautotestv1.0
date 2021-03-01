3c6bfe54d1c57ae8ae67787a2e5dbd00d51337ed
易首全 <15989434843@163.com>
2021-03-01 17:35:53 +0800

    继续优化用户相关接口用例代码

--- TestCase/UserRelatedapi/test_DeviceNext.py
+++ TestCase/UserRelatedapi/test_DeviceNext.py
@@ -36,33 +36,9 @@ class TestDeviceNext():
         }
 
         phone = "15811365600"
-        password = "zr123456"
+        # password = "zr123456"
         phoneArea = "86"
-        url = HTTP + "/as_user/api/user_account/v1/user_login_pwd"
-        paylo = {
-            "password": get_md5(password),
-            "phone": phone,
-            "phoneArea": phoneArea
-        }
-        sign1 = {"sign": get_sign(paylo)}  # 把参数签名后通过sign1传出来
-        payload1 = {}
-        payload1.update(paylo)
-        payload1.update(sign1)
 
-        payload = json.dumps(dict(payload1))
-
-        r = Requests(self.session).post(
-            url=url, headers=headers, data=payload, title="用户密码登陆"
-        )
-
-        j = r.json()
-        # 获取登录的token
-        # headers_token = j.get("data").get("token")
-        # headers1 = {}
-        # headers1.update(header)
-        # token = {"token": headers_token}
-        # # print(type(token))
-        # headers1.update(token)  # 将token更新到headers参数中
         boby = {
             "phone": phone,
             "countryCode": phoneArea
