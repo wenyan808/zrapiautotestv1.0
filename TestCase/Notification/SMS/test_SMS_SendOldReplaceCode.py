8303cc07ff04ecd6714074ece81d09ca0b50692a
易首全 <15989434843@163.com>
2021-04-09 17:51:32 +0800

    修改修改手机号发送短信验证码-当前手机号接口

--- TestCase/Notification/SMS/test_SMS_SendOldReplaceCode.py
+++ TestCase/Notification/SMS/test_SMS_SendOldReplaceCode.py
@@ -4,6 +4,7 @@ import json
 import allure
 import pytest
 
+from Common.getdevLoginToken import getlogintoken
 from Common.sign import get_sign
 
 from Common.requests_library import Requests
@@ -25,31 +26,11 @@ class TestSMSSendOldReplaceCode():
     # @pytest.mark.skip(reason="调试中 ")
     def test_SMS_SendOldReplaceCode(self):
         # 拼装参数
-        headers = JSON1
         phone = "15816144700"
         password = "zr123456"
-        url = HTTP + "/as_user/api/user_account/v1/user_login_pwd"
-        paylo = {
-            "password": get_md5(password),
-            "phone": phone,
-            "phoneArea": "86"
-        }
-        sign1 = {"sign": get_sign(paylo)}  # 把参数签名后通过sign1传出来
-        payload1 = {}
-        payload1.update(paylo)
-        payload1.update(sign1)
+        phoneArea = "86"
 
-        payload = json.dumps(dict(payload1))
-
-        r = Requests(self.session).post(
-            url=url, headers=headers, data=payload, title="用户密码登陆"
-        )
-
-        j = r.json()
-        # print(j)
-
-
-        headers_token = j.get("data").get("token")
+        headers_token = getlogintoken(phone, password, phoneArea)
         header = JSON1
         headers1 = {}
         headers1.update(header)
@@ -58,7 +39,7 @@ class TestSMSSendOldReplaceCode():
         headers1.update(token)  # 将token更新到headers
         boby = {
             "phone": phone,
-            "countryCode": "86"
+            "countryCode": phoneArea
         }
         sign1 = {"sign": get_sign(boby)}  # 把参数签名后通过sign1传出来
         payload1 = {}
@@ -70,7 +51,7 @@ class TestSMSSendOldReplaceCode():
             url=HTTP + "/as_notification/api/sms/v1/send_old_replace_code",
             headers=headers1, data=payload, title="修改手机号发送短信验证码-当前手机号"
         )
-        j =response_getdata.json()
+        j = response_getdata.json()
         assert response_getdata.status_code == 200
         if j.get("code") == "000000":
             assert j.get("msg") == "ok"
