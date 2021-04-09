99301b3b6904e924810546c448bf58a8da96ac18
易首全 <15989434843@163.com>
2021-04-09 17:44:09 +0800

    增加全局环境地址

--- glo.py
+++ glo.py
@@ -42,6 +42,54 @@ JSON = {
     "lang": "zh_CN"
 }
 
+# 测试环境开户登录账号
+loginAccount_phone = "15816262899"
+Accountlogin_password = "zr1234567"
+
 # 内网预发布环境192.168.1.121（Android）('15989434843', '123456QAZ', '86')
 # phone="15989434843"
 # pwd="123456QAZ"
+
+
+# 内网开发环境（dev）192.168.1.181（Android）
+http_dev = "http://192.168.1.181"
+phone_dev = "13418923886"  # 手机号
+pwd_dev = "zr123456"  # 密码
+clientId_dev = "281866727"
+passwordss = "271059"  # 卓锐交易密码
+zrNo_dev = '81866727'  # 卓锐号
+nickname_dev = '开车老司机'  # 卓锐用户昵称
+userId = "fa2022cf17064889a6cc7e4dce8c988b"
+
+JSON_dev = {
+    "Content-Type": "application/json",
+    "appVersion": '1.0',
+    "deviceId": "29DA6F9A-DD0F-4C03-A3FE-3C0563B7092E",
+    "osType": "ios",
+    "osVersion": '14.4',
+    "lang": "zh_CN"
+}
+consoledev_HTTP = "http://192.168.1.229:8080/apisC"
+loginAccountdev = "all"
+passworddev = "abcd12345"
+consoledev_JSON = {
+    "Content-Type": "application/json;charset=UTF-8",
+    "Connection": "keep-alive",
+    "traceId": "862c0ed0-5aef-11eb-8806-81a3e151bf57",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko)"
+                  " Chrome/86.0.4240.183 Safari/537.36",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.9",
+    "Cache-Control": "no-cache"
+}
+
+
+JSON2 = {
+    "Content-Type": "application/json",
+    "appVersion": '0.2.5',
+    "deviceId": "58CAB9A3-172A-43CB-9750-AAA6E45F2043",
+    "osType": "ios",
+    "osVersion": '14.4',
+    "lang": "zh_CN"
+}
