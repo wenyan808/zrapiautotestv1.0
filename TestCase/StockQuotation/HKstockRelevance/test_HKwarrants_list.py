54871d0dad7b2fa27fc3ed3d5e428e1705230ceb
陈家洛 <8637238+chen-jialuo@user.noreply.gitee.com>
2021-04-27 18:10:14 +0800

    修改窝轮牛熊在控制台有print打印

--- TestCase/StockQuotation/HKstockRelevance/test_HKwarrants_list.py
+++ TestCase/StockQuotation/HKstockRelevance/test_HKwarrants_list.py
@@ -16,7 +16,7 @@ class TestHKstockF10repolist:
         response = zhuorui('港股', '全部论证-窝轮牛熊列表涨跌幅排序')
         assert_data(response, '000000', 'ok')
         # assert response.status_code == 200
-        print(response.json())
+        # print(response.json())
     @allure.story('全部论证-窝轮牛熊列表涨跌价排序')
     def test_HKstock_warrantzdj(self):
         response = zhuorui('港股', '全部论证-窝轮牛熊列表涨跌价排序')
@@ -46,7 +46,7 @@ class TestHKstockF10repolist:
         response = zhuorui('港股', '全部论证-窝轮牛熊列表溢价率排序')
         # assert_data(response, '000000', 'ok')
         # assert response.status_code == 200
-        print(response.json())
+        # print(response.json())
     @allure.story('全部论证-窝轮牛熊列表到期日排序')
     def test_HKstock_warrantdqr(self):
         response = zhuorui('港股', '全部论证-窝轮牛熊列表到期日排序')
