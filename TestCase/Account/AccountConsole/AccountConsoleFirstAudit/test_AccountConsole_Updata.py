f10fc3d2142e14b91db48e9fa96c544d1d02d4e8
易首全 <15989434843@163.com>
2021-09-30 13:55:01 +0800

    新增开户console_修改信息接口

---
+++ TestCase/Account/AccountConsole/AccountConsoleFirstAudit/test_AccountConsole_Updata.py
@@ -0,0 +1,88 @@
+# test_AccountConsole_Updata       修改信息         /api/con_open/v1/update
+import json
+
+import allure
+
+from Business.IdentityInformation import signatureUrl, identityTypes
+from Common.Accountcommon.getAccountConsoleList import getAccountConsoleList, getAccountConsoleDetail, get_AuditItem
+
+from Common.getConsoleLogin import getConsoleLogin_token
+
+from Common.Accountcommon.getLoginAccountToken import GetLoginAccountToken
+
+from Common.Accountcommon.modify_submit_audit import modify_submit_audit
+
+from Common.show_sql import showsql
+
+from Common.sign import get_sign
+
+from Common.requests_library import Requests
+
+from glo import console_JSON, console_HTTP, loginAccount_phone, JSON, HTTP
+
+
+# @pytest.mark.skip(reason="调试中 ")
+@allure.feature('开户console_修改信息')
+class TestAccountConsoleUpdata():
+    @classmethod
+    def setup_class(cls) -> None:
+        cls.session = Requests().get_session()
+
+    def tearDown(self) -> None:
+        Requests(self.session).close_session()
+
+    # @pytest.mark.skip(reason="调试中 ")
+    def test_AccountConsole_Updata(self):
+        url1 = console_HTTP + "/api/con_open/v1/update"
+
+        headers = console_JSON
+        headers = headers
+        token = {"token": getConsoleLogin_token()}
+        headers.update(token)  # 将token更新到headers
+        # print(headers)
+        phone = loginAccount_phone
+
+        # identityTypes = [1]  # 身份类型 1-大陆居民 2-我是港澳地区居民 3-我是其他地区居民 海外居民，传入2,3
+        status = 2  # 状态，为空时查询所有数据 1、待客户修改 2、初审列表 3、终审列表 4、已通过 5、已拒绝
+        # print(getAccountConsoleList(headers, identityTypes, status, phone))
+        # 获取开户console列表
+        get_openOrderId = getAccountConsoleList(headers, identityTypes, status)
+        # print(get_openOrderId)
+        if len(get_openOrderId.get("data").get("list")) != 0:
+            openOrderId = get_openOrderId.get("data").get("list")[0].get("openOrderDTO").get("openOrderId")
+            # print(openOrderId)
+        else:
+            openOrderId = getAccountConsoleList(headers, identityTypes).get("data").get("list")[0].get(
+                "openOrderDTO").get("openOrderId")
+        # print(getAccountConsoleDetail(headers, openOrderId))
+        openId = getAccountConsoleDetail(headers, openOrderId).get("data").get("openInfoDTO").get("openId")  # 开户id
+
+        paylo = {
+            "openId": openId
+        }
+
+        sign1 = {"sign": get_sign(paylo)}  # 把参数签名后通过sign1传出来
+        payload1 = {}
+        payload1.update(paylo)
+        payload1.update(sign1)
+
+        payload = json.dumps(dict(payload1))
+
+        r1 = Requests(self.session).post(
+            url=url1, headers=headers, data=payload, title="修改信息"
+        )
+
+        k = r1.json()
+        # print(k)
+        assert r1.status_code == 200
+        if k.get("code") == "000000":
+            assert k.get("code") == "000000"
+            assert k.get("msg") == "ok"
+
+        else:
+            raise AssertionError(
+                f"\n请求地址：{url1}"
+                f"\nbody参数：{payload}"
+                f"\n请求头部参数：{headers}"
+                f"\n返回数据结果：{k}")
+
