c1c0db0d1eea08f0252e37df070fab1d9a511dea
易首全 <15989434843@163.com>
2021-11-01 18:29:39 +0800

    更新OpenAccount_conmmonPath.py

--- Business/OpenAccount_conmmonPath.py
+++ Business/OpenAccount_conmmonPath.py
@@ -1,3 +1,4 @@
+# app
 # 准备开户
 path_start = "/as_user/api/open/v1/start"
 # 账户选择
@@ -26,3 +27,21 @@ path_signature = "/as_user/api/cn_open/v1/signature"
 path_select_witness_card_type = "/as_user/api/cn_open/v1/select_witness_card_type"
 # 见证人信息（海外开户）
 path_witness = "/as_user/api/cn_open/v1/witness"
+# 查询开户状态（app使用）
+path_get_open_status = "/as_user/api/open/v1/get_open_status"
+# 修改开户方式(大陆+海外）
+path_modify_open_mode = "/as_user/api/cn_open/v1/modify_open_mode"
+# 修改个人签名(大陆+海外)
+path_modify_signature = "/as_user/api/cn_open/v1/modify_signature"
+# 修改资料提交审核
+path_modify_submit_audit = "/as_user/api/cn_open/v1/modify_submit_audit"
+# 查询审核数据
+path_get_open_audit = "/as_user/api/open/v1/get_open_audit"
+
+# console
+# 查询当前用户的审核数据
+path_conopen_get_open_audit = "/api/con_open/v1/get_open_audit"
+# 查询审核项
+path_get_audit_item = "/api/con_open/v1/get_audit_item"
+# 查询开户信息是否被占用
+path_select_exist_element = "/api/con_open/v1/select_exist_element"
