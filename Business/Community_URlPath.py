c89520631947b4012b68eaf11d72a697807c852b
易首全 <15989434843@163.com>
2021-11-01 18:34:48 +0800

    更新Comnunity_URLPath.py

---
+++ Business/Community_URlPath.py
@@ -0,0 +1,7 @@
+# 帖子管理(console)
+# 帖子列表
+URlPath_conPostList = "/api/con_post/v1/list"
+# app新增举报
+URlPath_addreport = "/as_community/api/report/v2/add"
+# 热门列表
+URlPath_hostlist = "/as_community/api/community/v1/hot_list"
